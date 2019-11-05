8209802e164ee1614ebfeaeeb9761549da842939
Zoe <hi@zoe.im>
2019-11-05 09:58:47 +0800

    :tada: add build scripts

--- main.go
+++ main.go
@@ -13,9 +13,26 @@ import (
 	_ "go.zoe.im/kops/cmd/mv/pv"
 )
 
+func init() {
+	cmd.Register(
+		cli.New(
+			cli.Name("version"),
+			cli.Short("Show version information."),
+			cli.Run(func(c *cli.Command, args ...string) {
+				fmt.Println("Version:", cmd.Version)
+				fmt.Println("Release-Tag:", cmd.ReleaseTag)
+				fmt.Println("Commit-ID:", cmd.CommitID)
+				fmt.Println()
+				fmt.Println("GOROOT:", cmd.GOROOT)
+				fmt.Println("GOPATH:", cmd.GOPATH)
+			}),
+		),
+	)
+}
+
 func main() {
 	if err := cmd.Run(
-		cli.Version(ReleaseTag),
+		cli.Version(cmd.ReleaseTag),
 	); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
